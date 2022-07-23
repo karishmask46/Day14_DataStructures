26553930fbd8b0ce32fc2b7e4f287fe6659a4726
karishmask46 <karishmask.15999@gmail.com>
2022-07-23 13:18:54 +0530

    Creating linkedList

---
+++ Day14_DataStructures/Day14_DataStructures/Program.cs
@@ -0,0 +1,7 @@
+﻿using Day14_DataStructures;
+Console.WriteLine("Hello World!");
+UC1_CreatingLinkedList list = new UC1_CreatingLinkedList();
+list.Add(56);
+list.Add(30);
+list.Add(70);
+list.Display();
